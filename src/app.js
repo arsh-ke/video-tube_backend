49ea26578491938936759ce33040aba06c032e0a
Arsh Kureshi <kureshimohammadarsh@gmail.com>
2026-01-21 23:52:30 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 import express from "express"
+import cors from "cors";
+
+app.use(cors({
+    origin: process.env.cors_origin,
+    Credential: true     
+}));
+
+app.use (express.json())
 const app = express()
 export default app
