a21adcf0d55d2a1093c94d20fedb6f0f1425d0fc
Arsh Kureshi <kureshimohammadarsh@gmail.com>
2026-01-21 23:47:17 +0530

    inject .env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import app from './app.js'
-const PORT = 8000;
-app.listen( () => {
+import dotenv from 'dotenv';
+import app from './app.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT ||3000;
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
+
