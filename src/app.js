a021de78316e9156c10ba96687671dc848cf4427
Arsh Kureshi <kureshimohammadarsh@gmail.com>
2026-01-24 22:56:21 +0530

    setup basic middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express"
 import cors from "cors";
 
+const app = express()
+
 app.use(cors({
     origin: process.env.cors_origin,
     Credential: true     
 }));
+//basic middleware
+app.use (express.json({limit: '50mb'}));
+app.use (express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static('public'));
 
-app.use (express.json())
-const app = express()
 export default app
