eb7d2da70d1e6d7473216089d2f5283efeb12fa6
Arsh Kureshi <kureshimohammadarsh@gmail.com>
2026-01-26 23:49:10 +0530

    fix app

--- src/app.js
+++ src/app.js
@@ -12,4 +12,10 @@ app.use (express.json({limit: '50mb'}));
 app.use (express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static('public'));
 
+//import routes
+import Healthcheck from "./controllers/Healthcheck.js";
+
+
+//routes
+app.use("/api/v1/healthcheck", Healthcheck)
 export default app
