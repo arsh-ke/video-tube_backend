26d986a3ac7bbcd5d3c3734d718a81591d29168e
Arsh Kureshi <kureshimohammadarsh@gmail.com>
2026-01-27 19:35:17 +0530

    add video model

--- src/modules/user.model.js
+++ src/modules/user.model.js
@@ -1,23 +1,53 @@
 import mongoose ,{Schema} from "mongoose";
 
 const userschema = new Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        index: true,
-        lowercase: true,
+   
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
     },
-    email: {
-        type: String,
-        unique:true,
-        lowercase: true,
-        required: true,
-        
-}
-    
+    {
+        timestamps: true
+    }
+)
 
-
-})
  export const User = mongoose.model("User", userschema);
