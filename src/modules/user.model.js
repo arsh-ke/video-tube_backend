a4b59e8ca47663d48703e753ee07b1d9de8608a3
Arsh Kureshi <kureshimohammadarsh@gmail.com>
2026-01-29 19:29:35 +0530

    add compare password

--- src/modules/user.model.js
+++ src/modules/user.model.js
@@ -53,6 +53,9 @@ userschema.pre("save", async function (next) {
 if(this.isModified("password"))return next();
 this.password = await bcrypt.hash(this.password, 10);
 next();
-
 });
+userschema.methods.ispasswordcorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
  export const User = mongoose.model("User", userschema);
